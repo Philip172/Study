fdc5d0074eb4f9144596e3d0ba87df76c185a5c0
Philip <74772920+Philip172@users.noreply.github.com>
2021-02-25 12:34:25 +0400

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Numerics;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool save = false;
+            BigInteger big = 2;
+            
+            while(!save)
+            {
+
+
+                try
+                {
+
+                    big*=big;
+                    Console.WriteLine("\n\n\n\n");
+                    Console.WriteLine(big);
+                }
+                catch(OutOfMemoryException)
+                {
+                    Console.WriteLine("ERROR MEMORY");
+                    save = true;
+                }
+                catch
+                {
+                    Console.WriteLine("ERROR");
+                    save = true;
+                }
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
